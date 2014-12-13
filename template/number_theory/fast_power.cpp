b45c73996adfdf733f27536fbe72741b4573590c
剑紫青天 <561484726@qq.com>
2014-12-13 15:16:48 +0800

    添加快速乘求快速幂模板并更新gcd&lcm模板

---
+++ template/number_theory/fast_power.cpp
@@ -0,0 +1,71 @@
+/* 快速幂取模算法模板
+ * 
+ * 版本：
+ * 快速乘求快速幂
+ *
+ * 注意：
+ * 本算法适用于非负整数
+ * 0^0 == 1
+ * 
+ * 题意：
+ * 给三个非负整数x, y, m
+ * 输出 x^y % m
+ * 
+ * 数据：
+ * x, y m代表三个非负整数，范围不超过long long
+ */
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LONG;
+
+//快速乘    求 a*b % mod
+LONG FastMul(LONG a, LONG b, LONG mod);
+
+//快速幂    求 a^b % mod
+LONG FastPow(LONG a, LONG b, LONG mod);
+
+int main()
+{
+    LONG x, y, m;
+    while (~scanf("%lld %lld %lld", &x, &y, &m))
+    {
+        printf("%lld\n", FastPow(x, y, m));
+    }
+    return 0;
+}
+
+LONG FastMul(LONG a, LONG b, LONG mod)
+{
+    LONG ans = 0;
+    a %= mod, b %= mod;
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans += a;
+            ans %= mod;
+        }
+        a <<= 1;
+        b >>= 1;
+        a %= mod;
+    }
+    return ans;
+}
+
+LONG FastPow(LONG a, LONG b, LONG mod)
+{
+    LONG ans = 1;
+    a %= mod;
+    while (b)
+    {
+        if (b & 1) ans = FastMul(ans, a, mod);
+        b >>= 1;
+        a = FastMul(a, a, mod);
+    }
+    return ans;
+}
--- template/number_theory/gcd_lcm.cpp
+++ template/number_theory/gcd_lcm.cpp
@@ -7,7 +7,7 @@
  * 本算法适用于正整数
  *
  * 题意：
- * 给两个正整数。
+ * 给两个正整数
  * 输出这两个正整数的最大公约数及最小公倍数
  * 
  * 数据：
@@ -22,7 +22,7 @@ using namespace std;
 
 typedef long long LONG;
 
-inline LONG Gcd(LONG a, LONG b);    //求最大公约数
+inline LONG Gcd(LONG a, LONG b);    //辗转相除法求最大公约数
 
 inline LONG Lcm(LONG a, LONG b);    //求最小公倍数
 
